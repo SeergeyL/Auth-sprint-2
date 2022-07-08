e19e2e67587feb992c880182c89670fd307e5fb6
SeergeyL <pstydy@inbox.ru>
2022-07-08 12:13:25 +0300

    change config class

--- src/config.py
+++ src/config.py
@@ -1,24 +1,35 @@
 import os
+from pydantic import BaseSettings, Field, BaseModel
 
 
-class BaseConfig:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+class OAuthProvider(BaseModel):
+    provider: str
+    client_id: str
+    client_secret: str
+    authorize_url: str
+    access_token_url: str
+    base_url: str
+
+
+class BaseConfig(BaseSettings):
+    SECRET_KEY: str = Field('top_secret', env='SECRET_KEY')
     
-    POSTGRES_HOST = os.environ.get('POSTGRES_HOST', '127.0.0.1')
-    POSTGRES_PORT = os.environ.get('POSTGRES_PORT', 5432)
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE')
+    POSTGRES_HOST: str = Field('127.0.0.1', env='POSTGRES_HOST')
+    POSTGRES_PORT: int = Field(5432, env='POSTGRES_PORT')
+    POSTGRES_USER: str = Field('postgres', env='POSTGRES_USER')
+    POSTGRES_PASSWORD: str = Field('123456', env='POSTGRES_PASSWORD')
+    POSTGRES_DATABASE: str = Field('auth_database', env='POSTGRES_DATABASE')
 
-    REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
-    REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+    REDIS_HOST: str = Field('127.0.0.1', env='REDIS_HOST')
+    REDIS_PORT: int = Field(6379, env='REDIS_PORT')
 
-    OAUTH_CREDENTIALS = {
-        'yandex': {
-            'client_id': os.environ.get('YANDEX_OAUTH_ID'),
-            'client_secret': os.environ.get('YANDEX_OAUTH_SECRET'),
-            'authorize_url': 'https://oauth.yandex.ru/authorize',
-            'access_token_url': 'https://oauth.yandex.ru/token',
-            'base_url': 'https://login.yandex.ru/'
-        }
+    OAUTH_PROVIDERS = {
+        'yandex': OAuthProvider(
+            provider='yandex',
+            client_id=os.environ.get('YANDEX_OAUTH_ID'),
+            client_secret=os.environ.get('YANDEX_OAUTH_SECRET'),
+            authorize_url='https://oauth.yandex.ru/authorize',
+            access_token_url='https://oauth.yandex.ru/token',
+            base_url='https://login.yandex.ru/'
+        )
     }
